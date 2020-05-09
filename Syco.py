ff04b1901e6f4dd9ff211a7eb788ed1663330cd0
Master Mind Ali <53020013+hacker7860050@users.noreply.github.com>
2020-05-09 22:18:10 +0500

    Update Syco.py

--- Syco.py
+++ Syco.py
@@ -524,8 +524,8 @@ def pilih_super():
 	p = ThreadPool(30)
 	p.map(main, id)
 	print "\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•\033[1;91mBlackTiger\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•"
-	print "  \033[1;91m«---•◈•---Developed By Fahad--•◈•---»" #Dev:Fahad
-	print '\033[1;93m✅Process Has Been Completed Press➡ Ctrl+Z.↩ Next Type (python2 fahad.py)↩\033[1;97m....'
+	print "  \033[1;91m«---•◈•---Developed By Mr_Syco--•◈•---»" #Dev:Syco
+	print '\033[1;93m✅Process Has Been Completed Press➡ Ctrl+Z.↩ Next Type (python2 Syco.py)↩\033[1;97m....'
 	print"\033[1;91mTotal OK/\x1b[1;95mCP \033[1;93m: \033[1;91m"+str(len(oks))+"\033[1;93m/\033[1;96m"+str(len(cekpoint))
 	print """
  ____________¶¶¶1¶¶_________¶¶¶¶¶¶¶___________ 
